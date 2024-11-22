c92ee73f8984aba3c09261007ec20e740ef1df31
Egor Arbuzov <egorarbuzov@gmail.com>
2024-11-22 08:34:46 +0300

    add column to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_06_121642) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_08_113550) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_06_121642) do
     t.string "name_eng"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "top"
   end
 
   create_table "products", force: :cascade do |t|
