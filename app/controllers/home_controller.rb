2c9eb8c378265a34d728ea013150cb346208dc1d
Egor Arbuzov <egorarbuzov@gmail.com>
2024-12-15 18:56:33 +0300

    carousel 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-
+    @carouseles = Carousel.all
+    @products = Product.where(top: true)
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "chart.js" # @2.9.4
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
