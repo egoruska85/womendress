35b9357b674c8484d726180abccaa81b0dac3be0
Egor Arbuzov <egorarbuzov@gmail.com>
2024-12-20 09:26:40 +0300

    add meta-tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
+  before_action :set_page_option
   def index
     @carouseles = Carousel.all
     @products = Product.where(top: true)
+  end
+
+  def set_page_option
+    @page_title = t('page_title').capitalize!
+    @page_description = t('page_description')
+    @page_keywords = t('page_keywords')
   end
 end
