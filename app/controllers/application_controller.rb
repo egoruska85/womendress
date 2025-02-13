644fd7a904340c4b13cd26adf4ba0a3556f3fb55
Egor Arbuzov <egorarbuzov@gmail.com>
2025-02-13 11:08:53 +0300

    first start(1)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   private
 
   def set_variable
-    if Sitedetail.last.logo != nil
-      @logo = Sitedetail.last.logo
-    end
+    #if Sitedetail.last.logo != nil
+    #  @logo = Sitedetail.last.logo
+    #end
     @top_categories = Category.where(top: true)
     @other_categories = Category.where(top: false)
     @q = Product.ransack(params[:q])
