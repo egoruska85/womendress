27cb862168ced793c4f1a32588575ef51b5f7bfd
Egor Arbuzov <egorarbuzov@gmail.com>
2025-02-13 13:44:33 +0300

    relation colors 2

--- db/migrate/20250213070325_create_colors_products.rb
+++ db/migrate/20250213070325_create_colors_products.rb
@@ -1,8 +1,8 @@
 class CreateColorsProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :colors_products do |t|
-      t.string :color_id
-      t.string :product_id
+      t.references :color, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20250213072119_remove_columnproduct_id_from_color.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnproductIdFromColor < ActiveRecord::Migration[7.0]
-  def change
-     remove_column :colors, :product_id
-  end
-end
